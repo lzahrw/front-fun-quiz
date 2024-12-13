5654a5c9a8f6b61e56af1b664f604aa0ca293470
SabaShamekhi <shamekhi.s2020@gmail.com>
2024-12-14 00:37:17 +0330

    removing styles folder adding FormComponent

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,42 @@
 
 
 
-function App() {
+
+import React from 'react';
+import FormComponent from './components/FormComponent';
+
+const App = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert('Form submitted!');
+  };
+
   return (
-    <>
-    <h3>
-      hi
-    </h3>
-    </>
+    <div>
+      <FormComponent
+        title="Sign in as Player"
+        fields={[
+          { id: 'username', name: 'username', type: 'text', label: 'Username', placeholder: 'Enter your username', required: true },
+          { id: 'password', name: 'password', type: 'password', label: 'Password', placeholder: 'Enter your password', required: true },
+        ]}
+        buttonText="Sign In"
+        onSubmit={handleSubmit}
+      />
+    </div>
   );
-}
+};
 
 export default App;
+
+
+// function App() {
+//   return (
+//     <>
+//     <h3>
+//       hi
+//     </h3>
+//     </>
+//   );
+// }
+
+// export default App;
