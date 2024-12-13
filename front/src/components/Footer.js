43d0e85244b177e5b58563fa69b7ac9bf172cf3a
SabaShamekhi <shamekhi.s2020@gmail.com>
2024-12-13 04:38:11 +0330

    footer component

---
+++ front/src/components/Footer.js
@@ -0,0 +1,23 @@
+// src/components/Footer.js
+import React from 'react';
+import '../styles/Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      
+      <nav>
+        <ul>
+        <li><a href="/about">About Us</a></li>
+          <li><a href="/contact">Contact</a></li>
+          <li><a href="/help">Help</a></li>
+                    
+          
+        </ul>
+        <p>© 2024 FunQuiz. All Rights Reserved.</p>
+      </nav>
+    </footer>
+  );
+};
+
+export default Footer;
